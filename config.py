7e849792a757c1d28536f3090c2f54e78a14cbf7
moaziziyan <mo.aziziyan@gmail.com>
2025-04-27 23:08:45 +0330

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
 ")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://moaziziyan:oRhDrW8BuoCNYSbc@cluster0.wvf00ip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "mongodb+srv://moaziziyan:oRhDrW8BuoCNYSbc@cluster0.wvf00ip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6937183461"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
